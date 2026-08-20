d69fcb518d25f2439a67ebf87410528c2a39770d
ksk243 <yyghrk6ygk@privaterelay.appleid.com>
2026-08-20 16:22:02 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,26 @@
 
 set -e
 
-cd /app
-
 export TZ=Asia/Tokyo
 
 export RUN_MODE=auto
 
+echo "================================"
+
+echo "Cloud Run Paper Trader START"
+
+date
+
+echo "================================"
+
+cd /app
+
 python paper_trader.py
+
+echo "================================"
+
+echo "Cloud Run Paper Trader END"
+
+date
+
+echo "================================"
